d23d8c62b2ac387a6988ec103897f98b0487fc5c
Schparky <3172830+Schparky@users.noreply.github.com>
2019-07-09 10:16:59 -0600

    add tests for Google Users destination

---
+++ googledest/users_test.go
@@ -0,0 +1,263 @@
+package googledest
+
+import (
+	"encoding/json"
+	"math/rand"
+	"reflect"
+	"strconv"
+	"testing"
+
+	personnel_sync "github.com/silinternational/personnel-sync"
+	admin "google.golang.org/api/admin/directory/v1"
+)
+
+func TestNewGoogleUsersDestination(t *testing.T) {
+	type args struct {
+		destinationConfig personnel_sync.DestinationConfig
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    personnel_sync.Destination
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewGoogleUsersDestination(tt.args.destinationConfig)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewGoogleUsersDestination() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewGoogleUsersDestination() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGoogleUsers_initGoogleAdminService(t *testing.T) {
+	type fields struct {
+		GoogleUsersConfig  GoogleUsersConfig
+		AdminService       admin.Service
+		BatchSizePerMinute int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &GoogleUsers{
+				GoogleUsersConfig:  tt.fields.GoogleUsersConfig,
+				AdminService:       tt.fields.AdminService,
+				BatchSizePerMinute: tt.fields.BatchSizePerMinute,
+			}
+			if err := g.initGoogleAdminService(); (err != nil) != tt.wantErr {
+				t.Errorf("GoogleUsers.initGoogleAdminService() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGoogleUsers_GetIDField(t *testing.T) {
+	type fields struct {
+		GoogleUsersConfig  GoogleUsersConfig
+		AdminService       admin.Service
+		BatchSizePerMinute int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &GoogleUsers{
+				GoogleUsersConfig:  tt.fields.GoogleUsersConfig,
+				AdminService:       tt.fields.AdminService,
+				BatchSizePerMinute: tt.fields.BatchSizePerMinute,
+			}
+			if got := g.GetIDField(); got != tt.want {
+				t.Errorf("GoogleUsers.GetIDField() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGoogleUsers_ForSet(t *testing.T) {
+	type fields struct {
+		GoogleUsersConfig  GoogleUsersConfig
+		AdminService       admin.Service
+		BatchSizePerMinute int
+	}
+	type args struct {
+		syncSetJson json.RawMessage
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &GoogleUsers{
+				GoogleUsersConfig:  tt.fields.GoogleUsersConfig,
+				AdminService:       tt.fields.AdminService,
+				BatchSizePerMinute: tt.fields.BatchSizePerMinute,
+			}
+			if err := g.ForSet(tt.args.syncSetJson); (err != nil) != tt.wantErr {
+				t.Errorf("GoogleUsers.ForSet() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGoogleUsers_ListUsers(t *testing.T) {
+	t.Skip("Skipping test because it requires integration with Google")
+	t.SkipNow()
+
+	testConfig, err := personnel_sync.LoadConfig("../cmd/config.json")
+	if err != nil {
+		t.Errorf("Failed to load test config, error: %s", err.Error())
+		t.FailNow()
+	}
+
+	type fields struct {
+		DestinationConfig personnel_sync.DestinationConfig
+		GoogleUsersConfig GoogleUsersConfig
+		AdminService      admin.Service
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []personnel_sync.Person
+		wantErr bool
+	}{
+		{
+			name: "test listing users",
+			fields: fields{
+				DestinationConfig: testConfig.Destination,
+			},
+			want: []personnel_sync.Person{
+				{
+					CompareValue: "user_one@example.com",
+					ID:           "",
+					Attributes: map[string]string{
+						"email":      "user_one@example.com",
+						"familyName": "two",
+						"fullName":   "user two",
+						"givenName":  "user",
+					},
+					DisableChanges: false,
+				},
+				{
+					CompareValue: "user_two@example.com",
+					ID:           "",
+					Attributes: map[string]string{
+						"email":      "user_two@example.com",
+						"familyName": "two",
+						"fullName":   "user two",
+						"givenName":  "user",
+					},
+					DisableChanges: false,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g, err := NewGoogleUsersDestination(tt.fields.DestinationConfig)
+			if err != nil {
+				t.Errorf("Failed to get new googleUsers instance, error: %s", err.Error())
+				t.FailNow()
+			}
+			got, err := g.ListUsers()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GoogleUsers.ListUsers() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GoogleUsers.ListUsers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGoogleUsers_ApplyChangeSet(t *testing.T) {
+	t.Skip("Skipping test because it requires integration with Google")
+	t.SkipNow()
+
+	testConfig, err := personnel_sync.LoadConfig("./config.json")
+	if err != nil {
+		t.Errorf("Failed to load test config, error: %s", err.Error())
+		t.FailNow()
+	}
+
+	type fields struct {
+		DestinationConfig personnel_sync.DestinationConfig
+	}
+	type args struct {
+		changes personnel_sync.ChangeSet
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   personnel_sync.ChangeResults
+	}{
+		{
+			name: "expect one updated",
+			want: personnel_sync.ChangeResults{
+				Created: uint64(0),
+				Updated: uint64(1),
+				Deleted: uint64(0),
+			},
+			fields: fields{
+				DestinationConfig: testConfig.Destination,
+			},
+			args: args{
+				changes: personnel_sync.ChangeSet{
+					Create: []personnel_sync.Person{},
+					Update: []personnel_sync.Person{
+						{
+							CompareValue: "user@example.com",
+							ID:           "",
+							Attributes: map[string]string{
+								"email":      "user@example.com",
+								"familyName": strconv.Itoa(rand.Intn(1000)),
+								"givenName":  "x",
+							},
+							DisableChanges: false,
+						},
+					},
+					Delete: []personnel_sync.Person{},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g, err := NewGoogleUsersDestination(tt.fields.DestinationConfig)
+			if err != nil {
+				t.Errorf("Failed to get new googleUsers instance, error: %s", err.Error())
+				t.FailNow()
+			}
+			eventLog := make(chan personnel_sync.EventLogItem, 50)
+			if got := g.ApplyChangeSet(tt.args.changes, eventLog); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GoogleUsers.ApplyChangeSet() = %v, want %v", got, tt.want)
+			}
+			close(eventLog)
+		})
+	}
+}
