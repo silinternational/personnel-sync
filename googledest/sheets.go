ab279ea3016dc67f57cdedf1c311b35b9221627d
Schparky <3172830+Schparky@users.noreply.github.com>
2020-05-20 13:10:43 -0600

    writes data to sheet, but doesn't erase rows beyond update range

--- googledest/sheets.go
+++ googledest/sheets.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"sync/atomic"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
@@ -14,11 +15,15 @@ import (
 	personnel_sync "github.com/silinternational/personnel-sync"
 )
 
+// MaxColumns specifies the maximum number of columns (fields) copied into the sheet
+const MaxColumns = 40
+
 type GoogleSheets struct {
 	DestinationConfig personnel_sync.DestinationConfig
 	GoogleConfig      GoogleConfig
 	Client            http.Client
 	SheetsSyncSet     SheetsSyncSet
+	lastRow           int
 }
 
 type SheetsSyncSet struct {
@@ -92,60 +97,11 @@ func (g *GoogleSheets) ForSet(syncSetJson json.RawMessage) error {
 }
 
 func (g *GoogleSheets) ListUsers() ([]personnel_sync.Person, error) {
-
-	srv, err := sheets.New(&g.Client)
-	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
-	}
-
-	spreadsheetID := g.SheetsSyncSet.SheetID
-	readRange := "Sheet1!A2:E"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
-	}
-
-	if len(resp.Values) == 0 {
-		fmt.Println("No data found.")
-	} else {
-		fmt.Println("Name, Major:")
-		for _, row := range resp.Values {
-			// Print columns A and E, which correspond to indices 0 and 4.
-			fmt.Printf("%s, %s\n", row[0], row[4])
-		}
-	}
-
-	//var membersList []*admin.Member
-	//membersListCall := g.AdminService.Members.List(g.SheetsSyncSet.GroupEmail)
-	//err := membersListCall.Pages(context.TODO(), func(members *admin.Members) error {
-	//	membersList = append(membersList, members.Members...)
-	//	return nil
-	//})
-	//if err != nil {
-	//	return []personnel_sync.Person{}, fmt.Errorf("unable to get members of group %s: %s", g.SheetsSyncSet.GroupEmail, err.Error())
-	//}
-
 	var members []personnel_sync.Person
-	//
-	//for _, nextMember := range membersList {
-	//	// Do not include Extra Managers or ExtraOwners in list to prevent inclusion in delete list
-	//	if isExtraManager, _ := personnel_sync.InArray(nextMember.Email, g.SheetsSyncSet.ExtraManagers); isExtraManager {
-	//		continue
-	//	}
-	//	if isExtraOwner, _ := personnel_sync.InArray(nextMember.Email, g.SheetsSyncSet.ExtraOwners); isExtraOwner {
-	//		continue
-	//	}
-	//	if isExtraMember, _ := personnel_sync.InArray(nextMember.Email, g.SheetsSyncSet.ExtraMembers); isExtraMember {
-	//		continue
-	//	}
-	//
-	//	members = append(members, personnel_sync.Person{
-	//		CompareValue: nextMember.Email,
-	//		Attributes: map[string]string{
-	//			"Email": strings.ToLower(nextMember.Email),
-	//		},
-	//	})
-	//}
+
+	g.readSheet()
+
+	// To start with, let's just ignore the existing content and overwrite the entire sheet
 
 	return members, nil
 }
@@ -155,120 +111,124 @@ func (g *GoogleSheets) ApplyChangeSet(
 	eventLog chan<- personnel_sync.EventLogItem) personnel_sync.ChangeResults {
 
 	var results personnel_sync.ChangeResults
-	//var wg sync.WaitGroup
-	//
-	//// key = email, value = role
-	//toBeCreated := map[string]string{}
-	//for _, person := range changes.Create {
-	//	toBeCreated[person.CompareValue] = RoleMember
-	//}
-	//
-	//// Update Owner / Manager roles
-	//for _, owner := range g.SheetsSyncSet.Owners {
-	//	if _, ok := toBeCreated[owner]; ok {
-	//		toBeCreated[owner] = RoleOwner
-	//	}
-	//}
-	//for _, manager := range g.SheetsSyncSet.Managers {
-	//	if _, ok := toBeCreated[manager]; ok {
-	//		toBeCreated[manager] = RoleManager
-	//	}
-	//}
-	//
-	//// Add any ExtraManagers, ExtraOwners, and ExtraMembers to Create list since they are not in the source people
-	//for _, manager := range g.SheetsSyncSet.ExtraManagers {
-	//	toBeCreated[manager] = RoleManager
-	//}
-	//for _, owner := range g.SheetsSyncSet.ExtraOwners {
-	//	toBeCreated[owner] = RoleOwner
-	//}
-	//for _, member := range g.SheetsSyncSet.ExtraMembers {
-	//	toBeCreated[member] = RoleMember
-	//}
-	//
-	//// One minute per batch
-	//batchTimer := personnel_sync.NewBatchTimer(g.BatchSize, g.BatchDelaySeconds)
-	//
-	//if !g.SheetsSyncSet.DisableAdd {
-	//	for email, role := range toBeCreated {
-	//		wg.Add(1)
-	//		go g.addMember(email, role, &results.Created, &wg, eventLog)
-	//		batchTimer.WaitOnBatch()
-	//	}
-	//}
-	//
-	//if !g.SheetsSyncSet.DisableDelete {
-	//	for _, dp := range changes.Delete {
-	//		// Do not delete ExtraManagers, ExtraOwners, or ExtraMembers
-	//		if isExtraManager, _ := personnel_sync.InArray(dp.CompareValue, g.SheetsSyncSet.ExtraManagers); isExtraManager {
-	//			continue
-	//		}
-	//		if isExtraOwner, _ := personnel_sync.InArray(dp.CompareValue, g.SheetsSyncSet.ExtraOwners); isExtraOwner {
-	//			continue
-	//		}
-	//		if isExtraMember, _ := personnel_sync.InArray(dp.CompareValue, g.SheetsSyncSet.ExtraMembers); isExtraMember {
-	//			continue
-	//		}
-	//		wg.Add(1)
-	//		go g.removeMember(dp.CompareValue, &results.Deleted, &wg, eventLog)
-	//		batchTimer.WaitOnBatch()
-	//	}
-	//}
-	//
-	//wg.Wait()
+	var wg sync.WaitGroup
+
+	// One minute per batch
+	batchTimer := personnel_sync.NewBatchTimer(g.GoogleConfig.BatchSize, g.GoogleConfig.BatchDelaySeconds)
+
+	if !g.SheetsSyncSet.DisableAdd {
+		for _, person := range changes.Create {
+			wg.Add(1)
+			go g.addRow(person, &results.Created, &wg, eventLog)
+			batchTimer.WaitOnBatch()
+		}
+	}
+
+	wg.Wait()
 
 	return results
 }
 
-func (g *GoogleSheets) addMember(
-	email, role string,
+func (g *GoogleSheets) addRow(
+	person personnel_sync.Person,
 	counter *uint64,
 	wg *sync.WaitGroup,
 	eventLog chan<- personnel_sync.EventLogItem) {
 
 	defer wg.Done()
 
-	//newMember := admin.Member{
-	//	Role:  role,
-	//	Email: email,
-	//}
-	//
-	//_, err := g.AdminService.Members.Insert(g.SheetsSyncSet.GroupEmail, &newMember).Do()
-	//if err != nil && !strings.Contains(err.Error(), "409") { // error code 409 is for existing user
-	//	eventLog <- personnel_sync.EventLogItem{
-	//		Event:   "error",
-	//		Message: fmt.Sprintf("unable to insert %s in Google group %s: %s", email, g.SheetsSyncSet.GroupEmail, err.Error())}
-	//	return
-	//}
-	//
-	//eventLog <- personnel_sync.EventLogItem{
-	//	Event:   "AddMember",
-	//	Message: email,
-	//}
-	//
-	//atomic.AddUint64(counter, 1)
+	srv, err := sheets.New(&g.Client)
+	if err != nil {
+		eventLog <- personnel_sync.EventLogItem{
+			Event:   "error",
+			Message: fmt.Sprintf("Unable to retrieve Sheets client, error: %v", err),
+		}
+		return
+	}
+
+	if g.lastRow == 0 {
+		headerRow := make([]interface{}, MaxColumns)
+		i := 0
+		for key := range person.Attributes {
+			headerRow[i] = key
+			i++
+		}
+		for ; i < MaxColumns; i++ {
+			headerRow[i] = ""
+		}
+		v := &sheets.ValueRange{
+			Values: [][]interface{}{headerRow},
+		}
+
+		_, err := srv.Spreadsheets.Values.Update(
+			g.SheetsSyncSet.SheetID,
+			"Sheet1!A1:ZZ",
+			v).ValueInputOption("RAW").Do()
+		if err != nil {
+			eventLog <- personnel_sync.EventLogItem{
+				Event:   "error",
+				Message: fmt.Sprintf("Unable to add row to sheet, error: %v", err),
+			}
+			return
+		}
+
+		g.lastRow++
+	}
+
+	newRow := make([]interface{}, MaxColumns)
+	i := 0
+	for _, val := range person.Attributes {
+		newRow[i] = val
+		i++
+	}
+	for ; i < MaxColumns; i++ {
+		newRow[i] = ""
+	}
+	v := &sheets.ValueRange{
+		Values: [][]interface{}{newRow},
+	}
+
+	newRowRange := fmt.Sprintf("Sheet1!A%d:ZZ", g.lastRow+1)
+	fmt.Printf(newRowRange)
+	_, err = srv.Spreadsheets.Values.Update(
+		g.SheetsSyncSet.SheetID,
+		newRowRange,
+		v).ValueInputOption("RAW").Do()
+	if err != nil {
+		eventLog <- personnel_sync.EventLogItem{
+			Event:   "error",
+			Message: fmt.Sprintf("Unable to add row to sheet, error: %v", err),
+		}
+		return
+	}
+	g.lastRow++
+
+	eventLog <- personnel_sync.EventLogItem{
+		Event:   "AddMember",
+		Message: person.CompareValue,
+	}
+
+	atomic.AddUint64(counter, 1)
 }
 
-func (g *GoogleSheets) removeMember(
-	email string,
-	counter *uint64,
-	wg *sync.WaitGroup,
-	eventLog chan<- personnel_sync.EventLogItem) {
+func (g *GoogleSheets) readSheet() {
+	srv, err := sheets.New(&g.Client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client, error: %v", err)
+	}
 
-	defer wg.Done()
-
-	//err := g.AdminService.Members.Delete(g.SheetsSyncSet.GroupEmail, email).Do()
-	//if err != nil {
-	//	eventLog <- personnel_sync.EventLogItem{
-	//		Event:   "error",
-	//		Message: fmt.Sprintf("unable to delete %s from Google group %s: %s", email, g.SheetsSyncSet.GroupEmail, err.Error())}
-	//	return
-	//}
-	//
-	//eventLog <- personnel_sync.EventLogItem{
-	//	Event:   "RemoveMember",
-	//	Message: email,
-	//}
-	//
-	//atomic.AddUint64(counter, 1)
+	spreadsheetID := g.SheetsSyncSet.SheetID
+	readRange := "Sheet1!A1:E"
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet, error: %v", err)
+	}
+	if len(resp.Values) > 0 {
+		for _, row := range resp.Values {
+			for _, col := range row {
+				fmt.Printf("%s, ", col)
+			}
+			fmt.Printf("\n")
+		}
+	}
 }
