acfc640b0ed34c12be4beec1013e3c58c97f7689
Schparky <3172830+Schparky@users.noreply.github.com>
2020-05-21 14:12:00 -0600

    specify v3 for the personnel_sync import

--- googledest/sheets.go
+++ googledest/sheets.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 
-	personnel_sync "github.com/silinternational/personnel-sync"
+	personnel_sync "github.com/silinternational/personnel-sync/v3"
 )
 
 type GoogleSheets struct {
