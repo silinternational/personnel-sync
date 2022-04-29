53c30926e9213058e79427e2ec10a4e344b52224
Schparky <3172830+Schparky@users.noreply.github.com>
2022-04-29 12:05:12 -0600

    refactor LoadConfig and add a unit test

--- internal/internal_test.go
+++ internal/internal_test.go
@@ -6,7 +6,51 @@ import (
 	"reflect"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
+
+func TestReadConfig(t *testing.T) {
+	goodConfig := NewAppConfig()
+	goodConfig.Source.Type = "RestAPI"
+	goodConfig.Destination.Type = "RestAPI"
+	goodConfig.AttributeMap = []AttributeMap{{}}
+
+	tests := []struct {
+		name    string
+		config  string
+		want    AppConfig
+		wantErr bool
+	}{
+		{
+			name:    "successful unmarshal",
+			config:  `{"Source": {"Type": "RestAPI"},"Destination": {"Type": "RestAPI"},"AttributeMap":[{}]}`,
+			want:    goodConfig,
+			wantErr: false,
+		},
+		{
+			name:    "json parse error",
+			config:  `{"Runtime":{"Verbosity":"should be a number"}}`,
+			wantErr: true,
+		},
+		{
+			name:    "missing Source",
+			config:  `{"Destination": {"Type": "RestAPI"},"AttributeMap":[{}]}`,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ReadConfig([]byte(tt.config))
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
 
 func TestGenerateChangeSet(t *testing.T) {
 	type args struct {
