5208533e51ed5b2af78c664141256b404feabd13
Schparky <3172830+Schparky@users.noreply.github.com>
2022-05-02 10:27:27 -0600

    extract a Validate function for Config and add a unit test

--- internal/config_test.go
+++ internal/config_test.go
@@ -47,3 +47,53 @@ func TestReadConfig(t *testing.T) {
 		})
 	}
 }
+
+func TestConfig_Validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		config  Config
+		wantErr string
+	}{
+		{
+			name:    "default config, missing some things",
+			config:  NewConfig(),
+			wantErr: "missing",
+		},
+		{
+			name: "missing Source",
+			config: Config{
+				Destination:  DestinationConfig{Type: "RestAPI"},
+				AttributeMap: []AttributeMap{{Required: false}},
+			},
+			wantErr: "missing a Source",
+		},
+		{
+			name: "missing Destination",
+			config: Config{
+				Source:       SourceConfig{Type: "RestAPI"},
+				AttributeMap: []AttributeMap{{Required: false}},
+			},
+			wantErr: "missing a Destination",
+		},
+		{
+			name: "missing AttributeMap",
+			config: Config{
+				Destination:  DestinationConfig{Type: "RestAPI"},
+				Source:       SourceConfig{Type: "RestAPI"},
+				AttributeMap: []AttributeMap{},
+			},
+			wantErr: "missing an AttributeMap",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.config.Validate()
+			if tt.wantErr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.wantErr)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
